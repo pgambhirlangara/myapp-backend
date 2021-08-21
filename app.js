d9a8c848f4a5e9953b2e9acf29b68b5f9193a9f8
yumi0127 <a13158y@gmail.com>
2021-08-21 15:49:03 -0700

    install mongoose

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const path = require("path");
+const mongoose = require("mongoose");
+
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.listen(5000, () => console.log("listning"));
+mongoose
+  .connect(
+    "mongodb+srv://yumi:HNYp6CMgzItJL9yA@cluster0.lbe7x.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(5000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
